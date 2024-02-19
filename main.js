288be77c4094793e32f26059e9f1132486d55fe2
MW_Dev <122593986+Muhamed-Walid@users.noreply.github.com>
2024-02-19 16:56:32 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,243 @@
+let header = `<nav>
+
+  <div id="more-icons">
+  
+  <a href="about.html">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336h24V272H216c-13.3 0-24-10.7-24-24s10.7-24 24-24h48c13.3 0 24 10.7 24 24v88h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H216c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z"/></svg>
+  <h4>عن الموقع</h4>
+  </a>
+  <br>
+  <a href="q&a.html">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M192 0c-41.8 0-77.4 26.7-90.5 64H64C28.7 64 0 92.7 0 128V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V128c0-35.3-28.7-64-64-64H282.5C269.4 26.7 233.8 0 192 0zm0 64a32 32 0 1 1 0 64 32 32 0 1 1 0-64zM105.8 229.3c7.9-22.3 29.1-37.3 52.8-37.3h58.3c34.9 0 63.1 28.3 63.1 63.1c0 22.6-12.1 43.5-31.7 54.8L216 328.4c-.2 13-10.9 23.6-24 23.6c-13.3 0-24-10.7-24-24V314.5c0-8.6 4.6-16.5 12.1-20.8l44.3-25.4c4.7-2.7 7.6-7.7 7.6-13.1c0-8.4-6.8-15.1-15.1-15.1H158.6c-3.4 0-6.4 2.1-7.5 5.3l-.4 1.2c-4.4 12.5-18.2 19-30.6 14.6s-19-18.2-14.6-30.6l.4-1.2zM160 416a32 32 0 1 1 64 0 32 32 0 1 1 -64 0z"/></svg>
+  <h4>سؤال و جواب</h4>
+  </a>
+  </div>
+
+   <a href="#" class="more-link" onclick="openMore()">
+   
+    <svg class="more" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"/></svg>
+    
+    <h4 class="moreTxt">اكثر</h4>
+   </a>
+   
+    <a href="azkar.html">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M0 256C0 114.6 114.6 0 256 0c33 0 64.6 6.3 93.6 17.7c7.4 2.9 11.5 10.7 9.8 18.4s-8.8 13-16.7 12.4c-4.8-.3-9.7-.5-14.6-.5c-114.9 0-208 93.1-208 208s93.1 208 208 208c4.9 0 9.8-.2 14.6-.5c7.9-.5 15 4.7 16.7 12.4s-2.4 15.5-9.8 18.4C320.6 505.7 289 512 256 512C114.6 512 0 397.4 0 256zM375.4 137.4c3.5-7.1 13.7-7.1 17.2 0l31.5 63.8c1.4 2.8 4.1 4.8 7.2 5.3l70.4 10.2c7.9 1.1 11 10.8 5.3 16.4l-50.9 49.6c-2.3 2.2-3.3 5.4-2.8 8.5l12 70.1c1.3 7.8-6.9 13.8-13.9 10.1l-63-33.1c-2.8-1.5-6.1-1.5-8.9 0l-63 33.1c-7 3.7-15.3-2.3-13.9-10.1l12-70.1c.5-3.1-.5-6.3-2.8-8.5L261 233.1c-5.7-5.6-2.6-15.2 5.3-16.4l70.4-10.2c3.1-.5 5.8-2.4 7.2-5.3l31.5-63.8z"/></svg>
+      <h4>الأذكار</h4>
+    </a>
+    
+    <a href="books.html">
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 122.88 101.37" style="enable-background:new 0 0 122.88 101.37" xml:space="preserve"><g><path d="M12.64,77.27l0.31-54.92h-6.2v69.88c8.52-2.2,17.07-3.6,25.68-3.66c7.95-0.05,15.9,1.06,23.87,3.76 c-4.95-4.01-10.47-6.96-16.36-8.88c-7.42-2.42-15.44-3.22-23.66-2.52c-1.86,0.15-3.48-1.23-3.64-3.08 C12.62,77.65,12.62,77.46,12.64,77.27L12.64,77.27z M103.62,19.48c-0.02-0.16-0.04-0.33-0.04-0.51c0-0.17,0.01-0.34,0.04-0.51V7.34 c-7.8-0.74-15.84,0.12-22.86,2.78c-6.56,2.49-12.22,6.58-15.9,12.44V85.9c5.72-3.82,11.57-6.96,17.58-9.1 c6.85-2.44,13.89-3.6,21.18-3.02V19.48L103.62,19.48z M110.37,15.6h9.14c1.86,0,3.37,1.51,3.37,3.37v77.66 c0,1.86-1.51,3.37-3.37,3.37c-0.38,0-0.75-0.06-1.09-0.18c-9.4-2.69-18.74-4.48-27.99-4.54c-9.02-0.06-18.03,1.53-27.08,5.52 c-0.56,0.37-1.23,0.57-1.92,0.56c-0.68,0.01-1.35-0.19-1.92-0.56c-9.04-4-18.06-5.58-27.08-5.52c-9.25,0.06-18.58,1.85-27.99,4.54 c-0.34,0.12-0.71,0.18-1.09,0.18C1.51,100.01,0,98.5,0,96.64V18.97c0-1.86,1.51-3.37,3.37-3.37h9.61l0.06-11.26 c0.01-1.62,1.15-2.96,2.68-3.28l0,0c8.87-1.85,19.65-1.39,29.1,2.23c6.53,2.5,12.46,6.49,16.79,12.25 c4.37-5.37,10.21-9.23,16.78-11.72c8.98-3.41,19.34-4.23,29.09-2.8c1.68,0.24,2.88,1.69,2.88,3.33h0V15.6L110.37,15.6z M68.13,91.82c7.45-2.34,14.89-3.3,22.33-3.26c8.61,0.05,17.16,1.46,25.68,3.66V22.35h-5.77v55.22c0,1.86-1.51,3.37-3.37,3.37 c-0.27,0-0.53-0.03-0.78-0.09c-7.38-1.16-14.53-0.2-21.51,2.29C79.09,85.15,73.57,88.15,68.13,91.82L68.13,91.82z M58.12,85.25 V22.46c-3.53-6.23-9.24-10.4-15.69-12.87c-7.31-2.8-15.52-3.43-22.68-2.41l-0.38,66.81c7.81-0.28,15.45,0.71,22.64,3.06 C47.73,78.91,53.15,81.64,58.12,85.25L58.12,85.25z"/></g></svg>
+      <h4>الكتب</h4>
+    </a>
+    
+    <a href="videos.html">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 128C0 92.7 28.7 64 64 64H320c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128zM559.1 99.8c10.4 5.6 16.9 16.4 16.9 28.2V384c0 11.8-6.5 22.6-16.9 28.2s-23 5-32.9-1.6l-96-64L416 337.1V320 192 174.9l14.2-9.5 96-64c9.8-6.5 22.4-7.2 32.9-1.6z"/></svg>
+      <h4 class="big-txt">قوائم دينية</h4>
+    </a>
+    
+    
+    <a href="quran.html">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M352 0c53 0 96 43 96 96V416c0 53-43 96-96 96H64 32c-17.7 0-32-14.3-32-32s14.3-32 32-32V384c-17.7 0-32-14.3-32-32V32C0 14.3 14.3 0 32 0H64 352zm0 384H96v64H352c17.7 0 32-14.3 32-32s-14.3-32-32-32zM274.1 150.2l-8.9 21.4-23.1 1.9c-5.7 .5-8 7.5-3.7 11.2L256 199.8l-5.4 22.6c-1.3 5.5 4.7 9.9 9.6 6.9L280 217.2l19.8 12.1c4.9 3 10.9-1.4 9.6-6.9L304 199.8l17.6-15.1c4.3-3.7 2-10.8-3.7-11.2l-23.1-1.9-8.9-21.4c-2.2-5.3-9.6-5.3-11.8 0zM96 192c0 70.7 57.3 128 128 128c25.6 0 49.5-7.5 69.5-20.5c3.2-2.1 4.5-6.2 3.1-9.7s-5.2-5.6-9-4.8c-6.1 1.2-12.5 1.9-19 1.9c-52.4 0-94.9-42.5-94.9-94.9s42.5-94.9 94.9-94.9c6.5 0 12.8 .7 19 1.9c3.8 .8 7.5-1.3 9-4.8s.2-7.6-3.1-9.7C273.5 71.5 249.6 64 224 64C153.3 64 96 121.3 96 192z"/></svg>
+      <h4 class="big-txt">القرآن الكريم</h4>
+    </a>
+    
+    <a href="index.html">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+              <path d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z"/></svg>
+      <h4>الرئيسية</h4>
+    </a>
+    <br>
+  </nav>
+  
+   <!-- Header -->
+   <header>
+      <h2 class="logo">صندوق مسلم</h2>
+    </header>
+  `;
+  let headerCont = document.querySelector('#headerCont');
+  headerCont.innerHTML = header;
+  
+  
+  
+  //Api
+  
+  function loadSurah(surahNumber) {
+    fetch(`https://api.alquran.cloud/v1/surah/${surahNumber}/uthmani`)
+      .then(response => response.json())
+      .then(data => {
+        const surah = data.data;
+        const surahContainer = document.createElement("div");
+        surahContainer.classList.add("surah-container");
+  
+        const surahHeader = document.createElement("div");
+        const surahInfo = document.createElement("div");
+        const surahTitle = document.createElement("h1");
+        surahTitle.classList.add("arabic-title");
+        surahTitle.textContent = `${surah.name}`;
+        surahInfo.appendChild(surahTitle);
+  
+        const surahNumberSpan = document.createElement("span");
+        surahNumberSpan.classList.add("surah-number");
+        surahNumberSpan.textContent = `(${surah.number})`;
+        surahInfo.appendChild(surahNumberSpan);
+  
+  
+        surahHeader.appendChild(surahInfo);
+  
+        const englishSurahTitle = document.createElement("h2");
+        englishSurahTitle.classList.add("english-subtitle");
+        englishSurahTitle.textContent = surah.englishName;
+        surahHeader.appendChild(englishSurahTitle);
+  
+        surahContainer.appendChild(surahHeader);
+  
+        const versesList = document.createElement("div");
+        surah.ayahs.forEach(verse => {
+          const verseItem = document.createElement("div");
+          verseItem.classList.add("verse");
+          const arabicVerseText = document.createElement("p");
+          arabicVerseText.classList.add("arabic-text");
+          arabicVerseText.textContent = verse.text;
+          verseItem.appendChild(arabicVerseText);
+  
+          const verseInfo = document.createElement("div");
+          const verseNumberSpan = document.createElement("span");
+          verseNumberSpan.classList.add("verse-number");
+          verseNumberSpan.textContent = `(${verse.numberInSurah})`;
+          verseInfo.appendChild(verseNumberSpan);
+  
+          const englishVerseText = document.createElement("p");
+          englishVerseText.classList.add("english-text");
+          englishVerseText.textContent = verse.englishName;
+          verseInfo.appendChild(englishVerseText);
+  
+          verseItem.appendChild(verseInfo);
+  
+          versesList.appendChild(verseItem);
+        });
+        surahContainer.appendChild(versesList);
+  
+        document.querySelector("#quran-container").appendChild(surahContainer);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+  
+  window.addEventListener('load', function() {
+    fetch("https://api.alquran.cloud/v1/surah")
+      .then((response) => response.json())
+      .then((data) => {
+        const surahs = data.data;
+        surahs.forEach((surah) => {
+          const surahNumber = surah.number;
+          const surahName = surah.name.charAt(0).toUpperCase() + surah.name.slice(1);
+          const surahListItem = document.createElement("li");
+          surahListItem.textContent = surahName;
+          surahListItem.classList.add("s");
+          surahListItem.onclick = () => {
+            document.querySelector("#quran-container").innerHTML = "";
+            loadSurah(surahNumber);
+          };
+          document.querySelector("#quran-container").appendChild(surahListItem);
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
+  
+// More Icon
+let moreBox = document.getElementById("more-icons");
+let moreIcon = document.querySelector('.more');
+let moreTxt = document.querySelector('.moreTxt');
+
+moreBox.style.bottom = "-200px";
+
+function openMore() {
+  if (moreBox.style.bottom === "-200px") {
+    moreTxt.textContent = "أقل";
+    moreBox.style.setProperty("bottom", "110px", "important");
+    moreIcon.style.transform = "rotateX(170deg)";
+  }
+  
+  else if(moreBox.style.bottom === "110px"){
+    moreTxt.textContent = "أكثر";
+    moreBox.style.setProperty("bottom", "-200px", "important");
+    moreIcon.style.transform = "rotateX(0)";
+  }
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  fetch("morning.json")
+    .then(response => response.json())
+    .then(data => {
+      const azkarContainer = document.getElementById("azkar-container");
+      data.forEach(azkar => {
+        const azkarDiv = document.createElement("div");
+        azkarDiv.classList.add("azkar");
+        azkarDiv.innerHTML = `
+              <p>العدد: ${azkar.count}</p>
+               <h2>${azkar.category}</h2>
+              
+              <p>${azkar.zekr}</p>
+              <p>${azkar.reference}</p>
+              <p class="zikr-desc">${azkar.description}</p>
+            `;
+        azkarContainer.appendChild(azkarDiv);
+      });
+    })
+    .catch(error => console.log("Error fetching azkar:", error));
+});
+
+
+
+
+// Night Azkar
+
+document.addEventListener("DOMContentLoaded", function () {
+      fetch("night.json")
+        .then(response => response.json())
+        .then(data => {
+          const azkarContainer = document.getElementById("n-azkar-container");
+          data.forEach(azkar => {
+            const azkarDiv = document.createElement("div");
+            azkarDiv.classList.add("azkar");
+            azkarDiv.innerHTML = `
+              <p>العدد: ${azkar.count}</p>
+               <h2>${azkar.category}</h2>
+              
+              <p>${azkar.zekr}</p>
+              <p>${azkar.reference}</p>
+              <p class="zikr-desc">${azkar.description}</p>
+            `;
+            azkarContainer.appendChild(azkarDiv);
+          });
+        })
+        .catch(error => console.log("Error fetching azkar:", error));
+    });
+    
+
+ // Sleep Azkar
+ 
+ document.addEventListener("DOMContentLoaded", function () {
+      fetch("sleep.json")
+        .then(response => response.json())
+        .then(data => {
+          const azkarContainer = document.getElementById("s-azkar-container");
+          data.forEach(azkar => {
+            const azkarDiv = document.createElement("div");
+            azkarDiv.classList.add("azkar");
+            azkarDiv.innerHTML = `
+
+               <h2>${azkar.category}</h2>
+               <p>${azkar.zekr}</p>
+              
+            `;
+            azkarContainer.appendChild(azkarDiv);
+          });
+        })
+        .catch(error => console.log("Error fetching azkar:", error));
+    });
